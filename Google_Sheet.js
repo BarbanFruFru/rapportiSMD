bb5095d0571ceca427b9bf5424ad5b96a96817b9
BarbanFruFru <matteoriserva0411@gmail.com>
2025-02-05 18:58:20 +0100

    Update Google_Sheet.js

--- Google_Sheet.js
+++ Google_Sheet.js
@@ -1,11 +1,18 @@
-const scriptURL = 'https://script.google.com/macros/s/AKfycbyJgnz7XVICPgi1Qa7oyBjQhv1GkJdGEm-A7QW7gK0BbjkFILBR3FughK8jnqM7N7r5/exec'
+const scriptURL = 'https://script.google.com/macros/s/AKfycbyonEeiFHAcCRNxxpqTEb-Oez4Y18gjy79xOqec9hu5pv6yu3263MxNHa1OyArZNfJMwQ/exec';
 
-const form = document.forms['ModuloArresto']
+const form = document.forms['ModuloArresto'];
 
 form.addEventListener('submit', e => {
-  e.preventDefault()
-  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-  .then(response => alert("Rapporto inviato correttamente."))
-  .then(() => { window.location.reload(); })
-  .catch(error => console.error('Errore!', error.message))
-})
+  e.preventDefault();
+  
+  fetch(scriptURL, { 
+    method: 'POST', 
+    body: new URLSearchParams(new FormData(form)) 
+  })
+  .then(response => response.text()) 
+  .then(data => {
+    alert("Rapporto inviato correttamente.");
+    window.location.reload(); // Ricarica la pagina dopo l'invio
+  })
+  .catch(error => console.error('Errore!', error.message));
+});
