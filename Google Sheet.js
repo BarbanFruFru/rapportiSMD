41f4b51524e99649a39b45d32f80f21442e57551
BarbanFruFru <matteoriserva0411@gmail.com>
2025-02-05 18:46:38 +0100

    Update Google Sheet.js

--- Google Sheet.js
+++ Google Sheet.js
@@ -4,8 +4,8 @@ const form = document.forms['ModuloArresto']
 
 form.addEventListener('submit', e => {
   e.preventDefault()
-  fetch(scriptURL, { method: 'POST', body: new FormDaya(form)})
-  .then(responde => alert("Rapporto inviato correttamente."))
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+  .then(response => alert("Rapporto inviato correttamente."))
   .then(() => { window.location.reload(); })
   .catch(error => console.error('Errore!', error.message))
 })
