dd2dcf5363fdc5ed05a418bcb5cbab4b7f0d219f
BarbanFruFru <matteoriserva0411@gmail.com>
2025-02-05 18:22:47 +0100

    Create Google Sheet.js

---
+++ Google Sheet.js
@@ -0,0 +1,11 @@
+const scriptURL = 'https://script.google.com/macros/s/AKfycbyonEeiFHAcCRNxxpqTEb-Oez4Y18gjy79xOqec9hu5pv6yu3263MxNHa1OyArZNfJMwQ/exec'
+
+const form = document.forms['contact-form']
+
+form.addEventListener('submint', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormDaya(form)})
+  .then(responde => alert("Rapporto inviato correttamente."))
+  .then(() => { window.location.reload(); })
+  .catch(error => console.error('Errore!', error.message))
+})
